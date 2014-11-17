575cd9eee5a1f6b208a5d3df4be9467a94b188ee
kai.hwang@gmail.com <kaihwang@nx3.priv>
2014-11-17 14:32:39 -0800

    seeding in the thalamus.

--- qsub_PPI.sh
+++ qsub_PPI.sh
@@ -3,8 +3,8 @@
 # script to submit PPI analysis to the grid
 
 #mkdir tmp;
-
-for Subject in 106 107 108 109 110 111 112 113 114 115 116 117; do
-	sed "s/s in 106/s in ${Subject}/g" < align_th_aseg.sh > tmp/q_align_th_aseg_${Subject}.sh
-	qsub -V -M kaihwang -m e -e ~/tmp -o ~/tmp tmp/q_align_th_aseg_${Subject}.sh
+#106
+for Subject in 107 108 109 110 111 112 113 114 115 116 117; do
+	sed "s/s in 106/s in ${Subject}/g" < PPI_tha_reg.sh > tmp/q_PPI_tha_reg_${Subject}.sh
+	qsub -V -M kaihwang -m e -e ~/tmp -o ~/tmp tmp/q_PPI_tha_reg_${Subject}.sh
 done
