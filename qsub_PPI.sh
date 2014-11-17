5df47b71b4891dfc721bb0f7ca379fcaebfad449
kai.hwang@gmail.com <kaihwang@nx3.priv>
2014-11-17 12:52:46 -0800

    align aseg masks.

--- qsub_PPI.sh
+++ qsub_PPI.sh
@@ -5,6 +5,6 @@
 #mkdir tmp;
 
 for Subject in 106 107 108 109 110 111 112 113 114 115 116 117; do
-	sed "s/s in 106/s in ${Subject}/g" < run_fs.sh > tmp/qPPI_fs_${Subject}.sh
-	qsub -V -M kaihwang -m e -e ~/tmp -o ~/tmp tmp/qPPI_fs_${Subject}.sh
+	sed "s/s in 106/s in ${Subject}/g" < align_th_aseg.sh > tmp/q_align_th_aseg_${Subject}.sh
+	qsub -V -M kaihwang -m e -e ~/tmp -o ~/tmp tmp/q_align_th_aseg_${Subject}.sh
 done
---
+++ tmp/q_align_th_aseg_117.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# script to align aseg from freesurfer parcellation into functional space.
+
+cd /home/despo/kaihwang/TRSETMS/FS_Subjects
+SUBJECTS_DIR=$(pwd);
+
+WD='/home/despo/kaihwang/TRSETMS/'
+
+for s in 117; do
+	
+	#convert aseg and reorient to RPI. freesurfer has this wired RSP orientation convention....	
+	cd $SUBJECTS_DIR/${s}/mri
+	mri_convert aseg.mgz aseg.nii.gz
+	mri_convert brain.mgz brain.nii.gz
+	
+	fslreorient2std brain.nii.gz brain.nii.gz
+	fslreorient2std aseg.nii.gz aseg.nii.gz
+	
+	fast -g -o brain_fast brain.nii.gz
+	fslmaths brain_fast_pve_2 -thr 0.5 -bin brain_fast_wmseg
+	
+	
+	flirt -in brain.nii.gz -ref $WD/${s}/PRE/PPI/mc_target_b.nii.gz -out FSbrain_to_func -omat FS_brain_to_func_initial.mat -dof 6
+	flirt -in brain.nii.gz -ref $WD/${s}/PRE/PPI/mc_target_b.nii.gz -out FSbrain_to_func -omat FS_brain_to_func.mat \
+	-interp spline -wmseg brain_fast_wmseg \
+	-cost bbr -init FS_brain_to_func_initial.mat -dof 6 -schedule ${FSLDIR}/etc/flirtsch/bbr.sch 
+	
+	
+	flirt -in aseg.nii.gz -ref $WD/${s}/PRE/PPI/mc_target_b.nii.gz \
+	-out $WD/${s}/PRE/PPI/aseg_align.nii.gz \
+	-init FS_brain_to_func.mat -applyxfm -interp nearestneighbour
+
+	3dcalc -a $WD/${s}/PRE/PPI/aseg_align.nii.gz -expr 'equals(a,10)+equals(a,49)' -prefix $WD/All_Masks/${s}_Thalamus.nii
+
+
+done
