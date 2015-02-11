ecf64843ff5d09c7ac60ae20db889850d0c7a9af
kai.hwang@gmail.com <kaihwang@nx5.priv>
2015-02-10 21:47:57 -0800

    fix the run mismatch for moving raw BOLD folders.

--- Organize_Raw.sh
+++ Organize_Raw.sh
@@ -38,20 +38,22 @@ for s in 106 ; do
 	if 	[ ! -d "${WD}/${s}/Raw" ]; then
 
 		mkdir ${WD}/${s}/Raw
-		ln -s ${Taraz_Raw}/${s}/PRE/*.tgz ${WD}/${s}/Raw/Raw.tgz
+		#ln -s ${Taraz_Raw}/${s}/PRE/*.tgz ${WD}/${s}/Raw/Raw.tgz
 
 	fi	
 
-	if [ -e ${WD}/${s}/Raw/Raw.tgz ]; then
-		tar xvf ${WD}/${s}/Raw/Raw.tgz -C ${WD}/${s}/Raw
+	if [ ! -e ${WD}/${s}/Raw/Raw.tgz ]; then
+		ln -s ${Taraz_Raw}/${s}/PRE/*.tgz ${WD}/${s}/Raw/Raw.tgz
+		tar xf ${WD}/${s}/Raw/Raw.tgz -C ${WD}/${s}/Raw
 		mv ${WD}/${s}/Raw/Despo_Lab*/* ${WD}/${s}/Raw
+		rm -rf ${WD}/${s}/Raw/Despo_Lab*
 	fi
 
 	cd ${WD}/${s}/Raw
 
 	i="1"
 	for d in $(ls -d *TRSE* | sort -V); do
-		mv ${d} run_${i}
+		mv ${d} run${i}
 		i=$[$i+1]
 	done
 
